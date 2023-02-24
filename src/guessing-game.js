b755bd8b6e1c1cd170ba4dd728b7335f081d174a
dongl1n <102480011+dongl1n@users.noreply.github.com>
2023-02-24 21:49:14 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
+    left;
+    rigth;
+    constructor() {
+    }
+    
     setRange(min, max) {
-
+      this.left=min;
+      this.rigth=max;
     }
 
     guess() {
-
+      this.g=Math.round((this.rigth-this.left)/2)+this.left;
+      return this.g;
     }
-
+    
     lower() {
-
+      this.setRange(this.left, this.g);
     }
 
     greater() {
-
+      this.setRange(this.g, this.rigth);
     }
 }
 
